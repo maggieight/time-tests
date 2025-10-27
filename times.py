a90aec2ead9199ae891f53df559fad2ac9f44c3d
Weifan-Zhang <Weifan.Zhang97@gmail.com>
2025-10-27 09:58:01 +0000

    The current modification does not correctly determine whether there is really an overlapping interval

--- times.py
+++ times.py
@@ -4,10 +4,17 @@ import datetime
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
-    d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
-    sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
+    total_seconds = (end_time_s - start_time_s).total_seconds()
+    interval_length = (total_seconds - gap_between_intervals_s * (number_of_intervals - 1)) / number_of_intervals
+
+    sec_range = [
+        (
+            start_time_s + datetime.timedelta(seconds=i * (interval_length + gap_between_intervals_s)),
+            start_time_s + datetime.timedelta(seconds=i * (interval_length + gap_between_intervals_s) + interval_length)
+        )
+        for i in range(number_of_intervals)
+    ]
+
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 
@@ -15,10 +22,20 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
-            high = min(end1, end2)
-            overlap_time.append((low, high))
+            t1_start = datetime.datetime.strptime(start1, "%Y-%m-%d %H:%M:%S")
+            t1_end = datetime.datetime.strptime(end1, "%Y-%m-%d %H:%M:%S")
+            t2_start = datetime.datetime.strptime(start2, "%Y-%m-%d %H:%M:%S")
+            t2_end = datetime.datetime.strptime(end2, "%Y-%m-%d %H:%M:%S")
+
+            low = max(t1_start, t2_start)
+            high = min(t1_end, t2_end)
+
+            if low < high:
+                overlap_time.append(
+                    (low.strftime("%Y-%m-%d %H:%M:%S"), high.strftime("%Y-%m-%d %H:%M:%S"))
+                )
     return overlap_time
+
 
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
